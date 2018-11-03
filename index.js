2fc736284d8bc2694b5ac929430efcd676c0b1a2
Greg Pfaff <greg.pfaff@razorfish.com>
2018-11-03 12:45:16 -0500

    Updates

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log('loaded');
-
-const getData = fetch('https://raw.githubusercontent.com/GPFAFF/leaderBoard/master/data.json')
-  .then((res) => res.json())
-  .then((data) => {
-    console.log('data:', data);
-  })
-
-console.log(getData);
