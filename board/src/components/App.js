da1e788e265556d6503ce3c6d9a8b38e558c5fe4
Greg Pfaff <greg.pfaff@razorfish.com>
2018-11-03 13:04:04 -0500

    adding updates to package and fixing imports

--- board/src/components/App.js
+++ board/src/components/App.js
@@ -36,7 +36,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Golf Leaderboards</h1>
+          <h1 className="App-title">Golf Leaderboard</h1>
         </header>
         <h2 className="App-intro">
           PGA DFS STANDINGS
