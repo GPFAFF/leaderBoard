178008f53903335f1d5e310aa0555ba71ba805f2
Greg Pfaff <greg.pfaff@razorfish.com>
2019-02-01 21:04:02 -0600

    adding proper p tag and this will be the comment moving forward!

--- board/src/components/Loader.js
+++ board/src/components/Loader.js
@@ -2,19 +2,16 @@ import React from 'react';
 import logo from '../logo.svg';
 import PropTypes from 'prop-types';
 
-// const baseUrl = process.env.PUBLIC_URL;
-
 const Loader = (props) => (
 
   <div className="loader">
-    {/* <img src={`${baseUrl}/img/fleur.svg`} alt="Loading..." /> */}
     <img
       src={logo}
       alt='loading...'
       className="App-logo"
     />
-
     <h2>{props.message}</h2>
+    <p>This is going to be the new commit</p>
   </div>
 );
 
