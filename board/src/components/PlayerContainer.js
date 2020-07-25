1e10d6711ca5fc460c4d60a29d5187f2ed15e80e
Greg Pfaff <greg.pfaff@silvercar.com>
2020-07-25 00:31:13 -0500

    adding new app (#10)

--- board/src/components/PlayerContainer.js
+++ board/src/components/PlayerContainer.js
@@ -1,49 +1,80 @@
-import React, { Component } from "react";
+import React from "react";
 import PlayerCard from "./PlayerCard";
 import Loader from "./Loader";
-import PropTypes from "prop-types";
 
-class PlayerContainer extends Component {
-
-  render() {
-
-    if (this.props.loading) {
-      return (
-        <Loader message="⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️ INCOMING SCORES ⛳️⛳️⛳️⛳️⛳️⛳️⛳️" />
-      )
-    }
-
-    const players = this.props.data.map((item, i) => {
-      const newPlayer = { ...item };
-      newPlayer.totalScore = newPlayer.tournaments.reduce((acc, currentValue) => {
-        return acc + currentValue.tournamentType * currentValue.tournamentScore;
-      }, 0)
-      return newPlayer;
-    }).sort((a, b) => {
-      return b.totalScore - a.totalScore;
-    });
-
-    function pointsBack(player) {
-      const leader = players[0].totalScore;
-      const playerScore = player.totalScore;
-      return (leader - playerScore).toFixed(2);
-    }
-
+const PlayerContainer = ({ data, loading }) => {
+  if (loading || !data) {
     return (
-      <div className="player_container">
-        {players.map((player, i) => {
-          return <PlayerCard key={i} value={i + 1}
-            pointsBack={pointsBack(player)} player={player}
-          />
-        })}
-      </div>
-    );
+      <Loader
+        message="⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️ INCOMING SCORES ⛳️⛳️⛳️⛳️⛳️⛳️⛳️"
+      />
+    )
   }
-}
 
-PlayerContainer.propTypes = {
-  loading: PropTypes.bool.isRequired,
-}
+  const players = data.map((item, i) => {
+    const newPlayer = { ...item };
+    newPlayer.totalScore = newPlayer.tournaments.reduce((acc, currentValue) => {
+      return acc + currentValue.tournamentType * currentValue.tournamentScore;
+    }, 0)
+    return newPlayer;
+  }).sort((a, b) => {
+    return b.totalScore - a.totalScore;
+  });
 
+  function pointsBack(player) {
+    const leader = players[0].totalScore;
+    const playerScore = player.totalScore;
+    return (leader - playerScore).toFixed(2);
+  }
+
+  return (
+    <div className="player_container">
+      {players.map((player, i) => (
+        <PlayerCard key={i} value={i + 1}
+          pointsBack={pointsBack(player)} player={player}
+        />
+      ))}
+    </div>
+  )
+}
 
 export default PlayerContainer;
+// class PlayerContainer extends Component {
+
+//   render() {
+
+//     if (this.props.loading) {
+//       return (
+//         <Loader message="⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️ INCOMING SCORES ⛳️⛳️⛳️⛳️⛳️⛳️⛳️" />
+//       )
+//     }
+
+//     const players = this.props.data.map((item, i) => {
+//       const newPlayer = { ...item };
+//       newPlayer.totalScore = newPlayer.tournaments.reduce((acc, currentValue) => {
+//         return acc + currentValue.tournamentType * currentValue.tournamentScore;
+//       }, 0)
+//       return newPlayer;
+//     }).sort((a, b) => {
+//       return b.totalScore - a.totalScore;
+//     });
+
+//     function pointsBack(player) {
+//       const leader = players[0].totalScore;
+//       const playerScore = player.totalScore;
+//       return (leader - playerScore).toFixed(2);
+//     }
+
+//     return (
+//       <div className="player_container">
+//         {players.map((player, i) => {
+//           return <PlayerCard key={i} value={i + 1}
+//             pointsBack={pointsBack(player)} player={player}
+//           />
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+// export default PlayerContainer;
