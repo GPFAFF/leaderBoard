a6a65e5d86966cf003f4381abe408ef89f7723b2
Greg Pfaff <greg.pfaff@silvercar.com>
2021-01-31 17:52:01 -0600

    update icons

--- board/src/components/PlayerCard.js
+++ board/src/components/PlayerCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import TotalScore from './TotalScore';
 
 const PlayerCard = ({ player, pointsBack, value }) => {
@@ -13,7 +13,7 @@ const PlayerCard = ({ player, pointsBack, value }) => {
         </h2>
         {icon
           ? <img className="player_row__icon" src={icon} alt="icon" />
-          : <img className="player_row__icon" src="https://www.placecage.com/g/300/300" alt="icon" />
+          : <img className="player_row__icon" src="https://github.com/GPFAFF/leaderBoard/blob/master/img/poop.jpg" alt="icon" />
         }
       </div>
       <TotalScore
@@ -24,37 +24,6 @@ const PlayerCard = ({ player, pointsBack, value }) => {
     </div>
   )
 }
-
-class PlayerCard extends Component {
-  render() {
-    const { user, icon, tournaments, tournamentPlaces } = this.props.player;
-    const { pointsBack, value } = this.props;
-
-    return (
-      <div className="player_row">
-        <div className="player_row__title">
-          <h2>
-            <span className='player_row__place'>{value}.</span>
-            {user}
-          </h2>
-          {icon
-            ? <img className="player_row__icon" src={icon} alt="icon" />
-            : <img className="player_row__icon" src="https://www.placecage.com/g/300/300" alt="icon" />
-          }
-        </div>
-        <TotalScore
-          pointsBack={pointsBack}
-          tournaments={tournaments}
-          tournamentPlaces={tournamentPlaces}
-        />
-      </div>
-    )
-  }
-}
-
-PlayerCard.propTypes = {
-
-};
 
 export default PlayerCard;
 
