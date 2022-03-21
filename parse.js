f480324ee6cfffc62bc2a3d6ced49fb65b0f225a
Greg Pfaff <greg.pfaff@razorfish.com>
2022-03-20 22:06:10 -0500

    add valspar scores

--- parse.js
+++ parse.js
@@ -28,7 +28,7 @@ const teamNames = [
 
 let second;
 try {
-  const data = fs.readFileSync("csv/players.csv", "utf8");
+  const data = fs.readFileSync("csv/valspar.csv", "utf8");
   second = data;
 } catch (err) {
   console.error(err);
