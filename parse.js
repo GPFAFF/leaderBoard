0f2b496ff0a9c116f6682f34cc3cad70c58c92c6
Greg Pfaff <greg.pfaff@razorfish.com>
2022-05-15 20:38:15 -0500

    Add schwab scores

--- parse.js
+++ parse.js
@@ -28,7 +28,7 @@ const teamNames = [
 
 let second;
 try {
-  const data = fs.readFileSync("csv/wells.csv", "utf8");
+  const data = fs.readFileSync("csv/schwab.csv", "utf8");
   second = data;
 } catch (err) {
   console.error(err);
