bc4365721f1021745c1ce43b7a551e9f8325d1c9
Greg Pfaff <greg.pfaff@razorfish.com>
2022-08-07 21:03:02 -0500

    add wyndham

--- parse.js
+++ parse.js
@@ -28,7 +28,7 @@ const teamNames = [
 
 let second;
 try {
-  const data = fs.readFileSync("csv/3m.csv", "utf8");
+  const data = fs.readFileSync("csv/wyndham.csv", "utf8");
   second = data;
 } catch (err) {
   console.error(err);
