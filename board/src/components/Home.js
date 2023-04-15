9f2a37e9c2ec8eb4b5127eafcee3474f74a3e48f
Greg Pfaff <greg.pfaff@gmail.com>
2023-04-14 20:29:54 -0500

    remove button

--- board/src/components/Home.js
+++ board/src/components/Home.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Header from '../components/Header';
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <img className="image-banner" src="https://raw.githubusercontent.com/GPFAFF/leaderBoard/master/img/golf-funny.jpg" alt="funny golf" />
+        <img
+          className="image-banner"
+          src="https://raw.githubusercontent.com/GPFAFF/leaderBoard/master/img/golf-funny.jpg"
+          alt="funny golf"
+        />
         <h2> Choose the season standings </h2>
-        <Link className="navigation-button" to="/season"> Season </Link>
-        <Link className="navigation-button" to="/fedex"> Fedex </Link>
+        <Link className="navigation-button" to="/season">
+          {" "}
+          Season{" "}
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
