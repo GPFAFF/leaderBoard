442405ddd27bb1767598e8f50ca9ffb01c192616
Greg Pfaff <greg.pfaff@gmail.com>
2023-06-20 18:45:03 -0500

    add us open scores

--- parse.js
+++ parse.js
@@ -26,7 +26,7 @@ const teamNames = [
 
 let second;
 try {
-  const data = fs.readFileSync("csv/canada.csv", "utf8");
+  const data = fs.readFileSync("csv/ussopen.csv", "utf8");
   second = data;
 } catch (err) {
   console.error(err);
@@ -126,7 +126,7 @@ const tournamentData = data
 
     return {
       name: item.EntryName,
-      points: item.Points,
+      points: item.Points * 3,
       rank: calculateRank(item.Rank),
     };
   })
