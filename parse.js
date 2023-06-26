aa796f82f341ad29c5ceb9c59893b66e11e0f8f2
Greg Pfaff <greg.pfaff@gmail.com>
2023-06-25 19:28:59 -0500

    add travelers scores

--- parse.js
+++ parse.js
@@ -26,7 +26,7 @@ const teamNames = [
 
 let second;
 try {
-  const data = fs.readFileSync("csv/ussopen.csv", "utf8");
+  const data = fs.readFileSync("csv/travelers.csv", "utf8");
   second = data;
 } catch (err) {
   console.error(err);
@@ -129,7 +129,7 @@ const tournamentData = data
 
     return {
       name: item.EntryName,
-      points: item.Points * 3,
+      points: item.Points,
       rank: calculateRank(item.Rank),
     };
   })
