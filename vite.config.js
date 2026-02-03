26a1b4e9475c5abe5df942c788ec28d927967d87
Greg <greg.pfaff@gmail.com>
2026-02-03 12:16:15 -0600

    config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/leaderBoard/", // 👈 REQUIRED for GitHub Pages
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: "dist",
-  },
+  base: "./",
 });
